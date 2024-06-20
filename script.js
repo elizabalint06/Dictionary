e6ca9c16d551988c4049e5fc0573a22a646fbb9d
elizabalint06 <eliza.tancau12@gmail.com>
2024-06-20 12:55:05 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+let dictionary = new Set();
+
+function saveWord() {
+    let word = document.getElementById('word').value.trim();
+    if (word !== '') {
+        dictionary.add(word.toLowerCase());
+        document.getElementById('word').value = '';
+        showMessage(`Word "${word}" added to dictionary.`);
+    } else {
+        showMessage('Please enter a word.');
+    }
+}
+
+function searchWord() {
+    let searchWord = document.getElementById('search')
+        .value.trim().toLowerCase();
+    if (dictionary.has(searchWord)) {
+        showMessage(`Word "${searchWord}" found in dictionary.`);
+    } else {
+        showMessage(`Word "${searchWord}" not found in dictionary.`);
+    }
+}
+
+function showMessage(message) {
+    let messageElement = document.getElementById('message');
+    messageElement.textContent = message;
+}
+
+document.getElementById('saveBtn').addEventListener('click', saveWord);         
+document.getElementById('searchBtn').addEventListener('click', searchWord);
